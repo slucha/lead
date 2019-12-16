200c006c8544650730da5a75cb64dc4153d58f98
slucha <hq_powerseller@gmx.de>
2019-12-16 14:30:16 +0100

    Update get.js

--- get.js
+++ get.js
@@ -1,10 +1,5 @@
 
-    fetch('https://ipapi.co/json/')
-.then(response => response.json())
-.then(data => {
-  console.log(data) // Prints result from `response.json()` in getRequest
-  const Url = window.location.href
-
+ 
 
 
 
